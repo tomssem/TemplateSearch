cdd3d470f320bef346c14800fb7594ef7d03af02
Tom <tfwnicholson@gmail.com>
2020-12-10 00:35:56 +0000

    Add more tests

--- src/SearchAlgos.hpp
+++ src/SearchAlgos.hpp
@@ -5,9 +5,9 @@
 namespace TemplateSearch {
     template <typename RandomAccessIterator,
               typename Compare=std::less<typename std::iterator_traits<RandomAccessIterator>::value_type>>
-    void quicksort(RandomAccessIterator begin, RandomAccessIterator end, Compare cmp = Compare()) {
+void quicksort(RandomAccessIterator begin, RandomAccessIterator end, Compare cmp = Compare()) {
 
-    }
+}
 
     int myFunc(int i);
 }
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,17 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
+#include "SearchAlgos.hpp"
+
 TEST_CASE("Check the greeting") {
     CHECK("hello" == "hello");
 }
 
 TEST_CASE("Very simple") {
     CHECK(3 == 3);
+}
+
+TEST_CASE("My func") {
+    CHECK(TemplateSearch::myFunc(2) == 0);
 }
 
